0899b708745bdc6aa8ec41d6fab74920a9a268cd
Antonina Kobzar <112172129+AntoninaKobzar@users.noreply.github.com>
2023-04-05 23:09:37 +0300

    first task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,18 @@
+
+import { Profile } from './Profile/Profile';
+// import css from './Profile/Profile.css';
+
 export const App = () => {
   return (
     <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+      // style={{
+      //   height: '100vh',
+      //   display: 'flex',
+      //   justifyContent: 'center',
+      //   alignItems: 'center',
+      //   fontSize: 40,
+      //   color: '#010101'
+      // }}
+    ><Profile/></div>
   );
 };
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,45 @@
+
+import PropTypes from 'prop-types';
+import css from './Profile.module.css';
+import user from 'components/user.json';
+
+export const Profile = ({username, tag, location, avatar, stats,}) => {
+    return(
+    <div className={css.profile}>
+        <div className={css.description}>
+            <img
+                src={user.avatar}
+                alt="User avatar"
+                    className={css.avatar}
+            />
+                <p className={css.name}>{user.username}</p>
+            <p className={css.tag}>{user.tag}</p>
+            <p className={css.location}>{user.location}</p>
+        </div>
+
+        <ul className={css.stats}>
+            <li className={css.statsItem}>
+                <span className={css.label}>Followers</span>
+                <span className={css.quantity}>{user.stats.followers}</span>
+            </li>
+            <li className={css.statsItem}>
+                <span className={css.label}>Views</span>
+                <span className={css.quantity}>{user.stats.views}</span>
+            </li>
+            <li className={css.statsItem}>
+                <span className={css.label}>Likes</span>
+                <span className={css.quantity}>{user.stats.likes}</span>
+            </li>
+        </ul>
+        </div>
+    );
+};
+Profile.propTypes = {
+    username: PropTypes.string,
+    tag: PropTypes.string,
+    location: PropTypes.string,
+    avatar: PropTypes.string,
+    stats: PropTypes.number,
+    
+    
+}
