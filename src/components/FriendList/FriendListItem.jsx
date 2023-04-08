711a01998d54e2f14f87d426b911e7c14f4fbc2d
Antonina Kobzar <112172129+AntoninaKobzar@users.noreply.github.com>
2023-04-08 10:27:11 +0300

    1

--- src/components/FriendList/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import css from './FriendList.module.css';
-import PropTypes, { bool } from 'prop-types';
+import PropTypes from 'prop-types';
 
 
 const FriendListItem = ({avatar, name, isOnline, id,}) => {
